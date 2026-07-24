621915226d9d7e3d3a40f8147406a6be2a5ee925
ACEVEDO MURILLO, OSCAR YESID (Servicio Externo en YPF) <SE30311@grupo.ypf.com>
2026-07-24 15:41:59 -0500

    Router fixes

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { RouterProvider } from '@tanstack/react-router'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { getRouter } from './router'
 
-// Obtenemos la instancia del router que ya tenías configurada
 const router = getRouter()
 
-// Extraemos el queryClient que definiste dentro del contexto de tu router
+// Asignamos el basepath para que coincida con el subdirectorio de GitHub Pages
+router.options.basepath = '/Pijamas'
+
 const queryClient = router.options.context.queryClient
 
-// Registrar el tipo del router para autocompletado de TypeScript
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
