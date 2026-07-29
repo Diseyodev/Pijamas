1074d560d4f0ad438c1c7e6cbf6a14f10857f864
Oscar Acevedo <oscar.acevedo@globant.com>
2026-07-29 11:32:46 -0500

    Add new references

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,13 +1,121 @@
+import r00031 from "@/assets/0003-1.jpg";
+import r00032 from "@/assets/0003-2.jpg";
+import r00033 from "@/assets/0003-3.jpg";
+import r00071 from "@/assets/0007-1.jpg";
+import r00072 from "@/assets/0007-2.jpg";
+import r00073 from "@/assets/0007-3.jpg";
+import r00074 from "@/assets/0007-4.jpg";
+import r00075 from "@/assets/0007-5.jpg";
+import r00076 from "@/assets/0007-6.jpg";
+import r00077 from "@/assets/0007-7.jpg";
+import r00078 from "@/assets/0007-8.jpg";
+import r00079 from "@/assets/0007-9.jpg";
+import r000710 from "@/assets/0007-10.jpg";
+import r000711 from "@/assets/0007-11.jpg";
+import r000712 from "@/assets/0007-12.jpg";
+import r00101 from "@/assets/0010-1.jpg";
+import r00102 from "@/assets/0010-2.jpg";
+import r00103 from "@/assets/0010-3.jpg";
+import r00104 from "@/assets/0010-4.jpg";
+import r00105 from "@/assets/0010-5.jpg";
+import r00106 from "@/assets/0010-6.jpg";
+import r00151 from "@/assets/0015-1.jpg";
+import r00152 from "@/assets/0015-2.jpg";
+import r00153 from "@/assets/0015-3.jpg";
+import r00154 from "@/assets/0015-4.jpg";
+import r00155 from "@/assets/0015-5.jpg";
+import r00156 from "@/assets/0015-6.jpg";
+import r00157 from "@/assets/0015-7.jpg";
+import r00161 from "@/assets/0016-1.jpg";
+import r00162 from "@/assets/0016-2.jpg";
+import r00163 from "@/assets/0016-3.jpg";
+import r00171 from "@/assets/0017-1.jpg";
+import r00172 from "@/assets/0017-2.jpg";
+import r00173 from "@/assets/0017-3.jpg";
+import r00174 from "@/assets/0017-4.jpg";
+import r00175 from "@/assets/0017-5.jpg";
+import r00201 from "@/assets/0020-1.jpg";
+import r00202 from "@/assets/0020-2.jpg";
+import r00203 from "@/assets/0020-3.jpg";
+import r00204 from "@/assets/0020-4.jpg";
+import r00205 from "@/assets/0020-5.jpg";
+import r00206 from "@/assets/0020-6.jpg";
+import r00261 from "@/assets/0026-1.jpg";
+import r00262 from "@/assets/0026-2.jpg";
+import r00271 from "@/assets/0027-1.jpg";
+import r00272 from "@/assets/0027-2.jpg";
+import r00281 from "@/assets/0028-1.jpg";
+import r00282 from "@/assets/0028-2.jpg";
+import r00291 from "@/assets/0029-1.jpg";
+import r00292 from "@/assets/0029-2.jpg";
+import r00293 from "@/assets/0029-3.jpg";
+import r00294 from "@/assets/0029-4.jpg";
+import r00295 from "@/assets/0029-5.jpg";
+import r00296 from "@/assets/0029-6.jpg";
+import r00297 from "@/assets/0029-7.jpg";
+import r00298 from "@/assets/0029-8.jpg";
+import r00301 from "@/assets/0030-1.jpg";
+import r00302 from "@/assets/0030-2.jpg";
+import r00311 from "@/assets/0031-1.jpg";
+import r00312 from "@/assets/0031-2.jpg";
+import r00313 from "@/assets/0031-3.jpg";
+import r00314 from "@/assets/0031-4.jpg";
+import r00315 from "@/assets/0031-5.jpg";
+import r00316 from "@/assets/0031-6.jpg";
+import r00317 from "@/assets/0031-7.jpg";
+import r00318 from "@/assets/0031-8.jpg";
+import r00319 from "@/assets/0031-9.jpg";
+import r003110 from "@/assets/0031-10.jpg";
+import r00321 from "@/assets/0032-1.jpg";
+import r00322 from "@/assets/0032-2.jpg";
+import r00323 from "@/assets/0032-3.jpg";
+import r00341 from "@/assets/0034-1.jpg";
+import r00342 from "@/assets/0034-2.jpg";
+import r00351 from "@/assets/0035-1.jpg";
+import r00352 from "@/assets/0035-2.jpg";
+import r0041 from "@/assets/0041.jpg";
+import r00421 from "@/assets/0042-1.jpg";
+import r00422 from "@/assets/0042-2.jpg";
+import r00423 from "@/assets/0042-3.jpg";
+import r00424 from "@/assets/0042-4.jpg";
+import r00425 from "@/assets/0042-5.jpg";
+import r00426 from "@/assets/0042-6.jpg";
+import r0044 from "@/assets/0044.jpg";
+import r00681 from "@/assets/0068-1.jpg";
+import r00682 from "@/assets/0068-2.jpg";
+import r00683 from "@/assets/0068-3.jpg";
+import r00711 from "@/assets/0071-1.jpg";
+import r00712 from "@/assets/0071-2.jpg";
+import r00713 from "@/assets/0071-3.jpg";
+import r00714 from "@/assets/0071-4.jpg";
+import r00721 from "@/assets/0072-1.jpg";
+import r00722 from "@/assets/0072-2.jpg";
+import r00771 from "@/assets/0077-1.jpg";
+import r00772 from "@/assets/0077-2.jpg";
+import r00773 from "@/assets/0077-3.jpg";
+import r00774 from "@/assets/0077-4.jpg";
 import m08 from "@/assets/M08.jpg";
 import m08B from "@/assets/M08B.jpg";
-import m08E1 from "@/assets/M08E1.jpg";
-import m08E2 from "@/assets/M08E2.jpg";
+import m08C from "@/assets/M08C.jpg";
+import m08E1 from "@/assets/M08E-1.jpg";
+import m08E2 from "@/assets/M08E-2.jpg";
 import m08EL from "@/assets/M08EL.jpg";
-import product2 from "@/assets/product-2.jpg";
-import product3 from "@/assets/product-3.jpg";
-import product4 from "@/assets/product-4.jpg";
-import product5 from "@/assets/product-5.jpg";
-import product6 from "@/assets/product-6.jpg";
+import me09 from "@/assets/ME09.jpg";
+import m101 from "@/assets/M10-1.jpg";
+import m102 from "@/assets/M10-2.jpg";
+import mE12 from "@/assets/ME12.jpg";
+import mE131 from "@/assets/ME13-1.jpg";
+import mE132 from "@/assets/ME13-2.jpg";
+import m151 from "@/assets/M15-1.jpg";
+import m152 from "@/assets/M15-2.jpg";
+import mE16 from "@/assets/ME16.jpg";
+import mE19 from "@/assets/ME19.jpg";
+import mE20 from "@/assets/ME20.jpg";
+import mE21 from "@/assets/ME21.jpg";
+import mE22 from "@/assets/ME22.jpg";
+import mE23 from "@/assets/ME23.jpg";
+import mE24 from "@/assets/ME24.jpg";
+
 
 export type Product = {
   id: string;
@@ -25,8 +133,44 @@ const SIZES = ["S", "M", "L", "XL"];
 const productImagesMap: Record<string, string[]> = {
   M08: [m08], 
   M08B: [m08B], 
+  M08C: [m08C], 
   M08E: [m08E1, m08E2],                  
   M08EL: [m08EL],                  
+  ME09: [me09],                  
+  M10: [m101,m102],                  
+  ME12: [mE12],                  
+  ME13: [mE131, mE132],                  
+  M15: [m151,m152],                  
+  ME16: [mE16],                  
+  ME19: [mE19],                  
+  ME20: [mE20],                  
+  ME21: [mE21],                  
+  ME22: [mE22],                  
+  ME23: [mE23],                  
+  ME24: [mE24],              
+  R0003: [r00031,r00032,r00033],
+  R0007: [r00071,r00072,r00073,r00074,r00075,r00076,r00077,r00078,r00079,r000710,r000711,r000712],
+  R0010: [r00101,r00102,r00103,r00104,r00105,r00106],
+  R0015: [r00151,r00152,r00153,r00154,r00155,r00156,r00157],
+  R0016: [r00161,r00162,r00163],
+  R0017: [r00171,r00172,r00173,r00174,r00175],
+  R0020: [r00201,r00202,r00203,r00204,r00205,r00206],
+  R0026: [r00261,r00262],
+  R0027: [r00271,r00272],
+  R0028: [r00281,r00282],
+  R0029: [r00291,r00292,r00293,r00294,r00295,r00296,r00297,r00298],
+  R0030: [r00301,r00302],
+  R0031: [r00311,r00312,r00313,r00314,r00315,r00316,r00317,r00318,r00319,r003110],
+  R0032: [r00321,r00322,r00323],
+  R0034: [r00341,r00342],
+  R0035: [r00351,r00352], 
+  R0041: [r0041],
+  R0042: [r00421,r00422,r00423,r00424,r00425,r00426],
+  R0044: [r0044],
+  R0068: [r00681,r00682,r00683],
+  R0071: [r00711,r00712,r00713,r00714],
+  R0072: [r00721,r00722],
+  R0077: [r00771,r00772,r00773,r00774]
 };
 
 // Imagen de respaldo por si no has subido ninguna para algún ID
@@ -40,26 +184,44 @@ const getProductImages = (id: string): string[] => {
 export const products: Product[] = [
   { id: "M08",   name: "Pijama Ref. M08",   description: "Short con camisa botones.", description2:"Tela piel de durazno.", price: 60000,  images: getProductImages("M08"),   category: "Pijamas",   sizes: ["M"] },
   { id: "M08B",  name: "Pijama Ref. M08B",  description: "Shorts con camisa bordada.", description2:"Tela piel de durazno.", price: 60000,  images: getProductImages("M08B"),  category: "Pijamas",   sizes: SIZES },
+  { id: "M08C",  name: "Pijama Ref. M08C",  description: "Pantalón con camisa botones.", description2:"Tela piel de durazno.", price: 80000,  images: getProductImages("M08C"),  category: "Pijamas",   sizes: ["S","M"] },
   { id: "M08E",  name: "Pijama Ref. M08E",  description: "Trio de short, top y camisa botones.", description2:"Tela piel de durazno.", price: 75000,  images: getProductImages("M08E"),  category: "Pijamas",   sizes:  ["L","XL"]  },
   { id: "M08EL", name: "Pijama Ref. M08EL", description: "Trio: Short con camisa y top.", description2:"Tela piel de durazno.", price: 75000,  images: getProductImages("M08EL"), category: "Pijamas",   sizes: SIZES },
-  { id: "M10",   name: "Pijama Ref. M10",   description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 22000,  images: getProductImages("M10"),   category: "Pijamas",   sizes: SIZES },
-  { id: "M15",   name: "Pijama Ref. M15",   description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 35000,  images: getProductImages("M15"),   category: "Pijamas",   sizes: SIZES },
-  { id: "ME02",  name: "Pijama Ref. ME02",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 40000,  images: getProductImages("ME02"),  category: "Especiales", sizes: SIZES },
-  { id: "ME02E", name: "Pijama Ref. ME02E", description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 52000,  images: getProductImages("ME02E"), category: "Especiales", sizes: SIZES },
-  { id: "ME04",  name: "Pijama Ref. ME04",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 30000,  images: getProductImages("ME04"),  category: "Especiales", sizes: SIZES },
-  { id: "ME08C", name: "Pijama Ref. ME08C", description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 85000,  images: getProductImages("ME08C"), category: "Especiales", sizes: SIZES },
-  { id: "ME09",  name: "Pijama Ref. ME09",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 40000,  images: getProductImages("ME09"),  category: "Especiales", sizes: SIZES },
-  { id: "ME12",  name: "Pijama Ref. ME12",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 50000,  images: getProductImages("ME12"),  category: "Especiales", sizes: SIZES },
-  { id: "ME13",  name: "Pijama Ref. ME13",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 35000,  images: getProductImages("ME13"),  category: "Especiales", sizes: SIZES },
-  { id: "ME16",  name: "Pijama Ref. ME16",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 35000,  images: getProductImages("ME16"),  category: "Especiales", sizes: SIZES },
-  { id: "ME17",  name: "Pijama Ref. ME17",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 55000,  images: getProductImages("ME17"),  category: "Especiales", sizes: SIZES },
-  { id: "ME18",  name: "Pijama Ref. ME18",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 80000,  images: getProductImages("ME18"),  category: "Especiales", sizes: SIZES },
-  { id: "ME19",  name: "Pijama Ref. ME19",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 45000,  images: getProductImages("ME19"),  category: "Especiales", sizes: SIZES },
-  { id: "ME20",  name: "Pijama Ref. ME20",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 45000,  images: getProductImages("ME20"),  category: "Especiales", sizes: SIZES },
-  { id: "ME21",  name: "Pijama Ref. ME21",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 45000,  images: getProductImages("ME21"),  category: "Especiales", sizes: SIZES },
-  { id: "ME22",  name: "Pijama Ref. ME22",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 55000,  images: getProductImages("ME22"),  category: "Especiales", sizes: SIZES },
-  { id: "ME23",  name: "Pijama Ref. ME23",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 75000,  images: getProductImages("ME23"),  category: "Especiales", sizes: SIZES },
-  { id: "ME24",  name: "Pijama Ref. ME24",  description: "Pijama femenina Perfect Pijamas.", description2:"Tela piel de durazno.", price: 110000, images: getProductImages("ME24"), category: "Especiales", sizes: SIZES },
+  { id: "ME09", name: "Pijama Ref. ME09", description: "Pantalón con blusa tiras.", description2:"Tela seda satinada.", price: 40000,  images: getProductImages("ME09"), category: "Pijamas",   sizes: ["S","M"] },
+  { id: "M10",   name: "Pijama Ref. M10",   description: "Batola con tiras.", description2:"Tela satín.", price: 25000,  images: getProductImages("M10"),   category: "Pijamas",   sizes:  ["S"]  },
+  { id: "ME12",   name: "Pijama Ref. ME12",   description: "Pantalón con top.", description2:"Tela piel de durazno.", price: 50000,  images: getProductImages("ME12"),   category: "Pijamas",   sizes:  ["S"]  },
+  { id: "ME13",   name: "Pijama Ref. ME13",   description: "Short con blusa tiras.", description2:"Tela piel de durazno.", price: 35000,  images: getProductImages("ME13"),   category: "Pijamas",   sizes:  ["S", "L"]  },
+  { id: "M15",   name: "Pijama Ref. M15",   description: "Pantalón con buso.", description2:"Tela piel de durazno.", price: 35000,  images: getProductImages("M15"),   category: "Pijamas",   sizes: ["2","4"] },
+  { id: "ME16", name: "Pijama Ref. ME16", description: "Batola.", description2:"Tela piel de durazno.", price: 35000,  images: getProductImages("ME16"),   category: "Pijamas",   sizes: SIZES },
+  { id: "ME19", name: "Pijama Ref. ME19", description: "Short con blusa tiras.", description2:"Tela fria.", price: 45000,  images: getProductImages("ME19"),   category: "Pijamas",   sizes: SIZES },
+  { id: "ME20", name: "Pijama Ref. ME20", description: "Short con camisa.", description2:"Tela rib.", price: 45000,  images: getProductImages("ME20"),   category: "Pijamas",   sizes: SIZES },
+  { id: "ME21", name: "Pijama Ref. ME21", description: "Batola.", description2:"Tela rib.", price: 45000,  images: getProductImages("ME21"),   category: "Pijamas",   sizes: SIZES },
+  { id: "ME22", name: "Pijama Ref. ME22", description: "Short con buso.", description2:"Tela algodón con textura.", price: 55000,  images: getProductImages("ME22"),   category: "Pijamas",   sizes: ["M","L","XL"] },
+  { id: "ME23", name: "Pijama Ref. ME23", description: "Pantalón con camisa.", description2:"Tela algodón con textura.", price: 75000,  images: getProductImages("ME23"),   category: "Pijamas",   sizes: SIZES },
+  { id: "ME24", name: "Pijama Ref. ME24", description: "Multiuso: Pantalón con buso.", description2:"Tela burda fría.", price: 110000,  images: getProductImages("ME24"),   category: "Pijamas",   sizes: SIZES },
+  { id: "0003", name: "Pijama Ref. 0003", description: "Pantalón con buso.", description2:"Tela piel de durazno.", price: 50000,  images: getProductImages("R0003"),   category: "Pijamas",   sizes:  ["S", "M", "L"] },
+  { id: "0007", name: "Pijama Ref. 0007", description: "Pantalón con camisa.", description2:"Tela piel de durazno.", price: 45000,  images: getProductImages("R0007"),   category: "Pijamas",   sizes:  SIZES },
+  { id: "0010", name: "Pijama Ref. 0010", description: "Capri con blusa esqueleto.", description2:"Tela piel de durazno.", price: 36000,  images: getProductImages("R0010"),   category: "Pijamas",   sizes:  SIZES },
+  { id: "0015", name: "Pijama Ref. 0015", description: "Short con blusa tiras.", description2:"Tela satín.", price: 35000,  images: getProductImages("R0015"), category: "Pijamas", sizes: SIZES },
+  { id: "0016", name: "Pijama Ref. 0016", description: "Pantalón con buso.", description2:"Tela piel de durazno.", price: 40000,  images: getProductImages("R0016"), category: "Pijamas", sizes:  SIZES },
+  { id: "0017", name: "Pijama Ref. 0017", description: "Enterizo.", description2:"Tela piel de durazno.", price: 30000,  images: getProductImages("R0017"), category: "Pijamas", sizes:  ["M","L","XL"] },
+  { id: "0020", name: "Pijama Ref. 0020", description: "Batola con panty.", description2:"Tela piel de durazno.", price: 30000,  images: getProductImages("R0020"), category: "Pijamas", sizes:  ["S","L"] },
+  { id: "0026", name: "Pijama Ref. 0026", description: "Pantalón con camisa corta.", description2:"Tela piel de durazno.", price: 40000,  images: getProductImages("R0026"), category: "Pijamas", sizes:  ["S","L","XL"] },
+  { id: "0027", name: "Pijama Ref. 0027", description: "Pantalón con camisa corta.", description2:"Tela rib.", price: 40000, images: getProductImages("R0027"), category: "Pijamas", sizes:  ["L","XL"] },
+  { id: "0028", name: "Pijama Ref. 0028", description: "Pantalón con buso.", description2:"Tela térmica.", price: 55000, images: getProductImages("R0028"), category: "Pijamas", sizes:  SIZES },
+  { id: "0029", name: "Pijama Ref. 0029", description: "Pantalón con blusa tiras.", description2:"Tela piel de durazo.", price: 35000, images: getProductImages("R0029"), category: "Pijamas", sizes:  SIZES },
+  { id: "0030", name: "Pijama Ref. 0030", description: "Short con blusa tiras.", description2:"Tela rib.", price: 38000, images: getProductImages("R0030"), category: "Pijamas", sizes:  ["S", "M", "L"] },
+  { id: "0031", name: "Pijama Ref. 0031", description: "Short con blusa tiras.", description2:"Tela piel de durazno.", price: 25000, images: getProductImages("R0031"), category: "Pijamas", sizes:  SIZES },
+  { id: "0032", name: "Pijama Ref. 0032", description: "Pantalón con camisa niño.", description2:"Tela piel de durazno.", price: 40000, images: getProductImages("R0032"), category: "Pijamas", sizes:  ["2-4","6-8","10-12"] },
+  { id: "0034", name: "Pijama Ref. 0034", description: "Pantalón con buso.", description2:"Tela térmica.", price: 48000, images: getProductImages("R0034"), category: "Pijamas", sizes: ["2-4","6-8","10-12","14-16"] },
+  { id: "0035", name: "Pijama Ref. 0035", description: "Pantalón con buso.", description2:"Tela térmica.", price: 48000, images: getProductImages("R0035"), category: "Pijamas", sizes: ["2-4","6-8","10-12","14-16"] },
+  { id: "0041", name: "Pijama Ref. 0041", description: "Short con camisa.", description2:"Tela piel de durazno.", price: 32000, images: getProductImages("R0041"), category: "Pijamas", sizes: ["L"] },
+  { id: "0042", name: "Pijama Ref. 0042", description: "Capri con camisa.", description2:"Tela piel de durazno.", price: 32000, images: getProductImages("R0042"), category: "Pijamas", sizes: ["S", "M"] },
+  { id: "0044", name: "Pijama Ref. 0044", description: "Pantaloneta.", description2:"Tela piel de durazno.", price: 24000, images: getProductImages("R0044"), category: "Pijamas", sizes: ["S"] },
+  { id: "0068", name: "Pijama Ref. 0068", description: "Pantalón con camisa caballero.", description2:"Tela piel de durazno.", price: 45000, images: getProductImages("R0068"), category: "Pijamas", sizes: SIZES },
+  { id: "0071", name: "Pijama Ref. 0071", description: "Pantalón con blusa tiras.", description2:"Tela piel de durazno.", price: 40000, images: getProductImages("R0071"), category: "Pijamas", sizes: SIZES },
+  { id: "0072", name: "Pijama Ref. 0072", description: "Capri con blusa tiras.", description2:"Tela piel de durazno.", price: 36000, images: getProductImages("R0072"), category: "Pijamas", sizes: ["S","L"] },
+  { id: "0077", name: "Pijama Ref. 0077", description: "Short con blusa tiras.", description2:"Tela satín.", price: 38000, images: getProductImages("R0077"), category: "Pijamas", sizes: SIZES },
 ];
 
 export const formatPrice = (n: number) =>
